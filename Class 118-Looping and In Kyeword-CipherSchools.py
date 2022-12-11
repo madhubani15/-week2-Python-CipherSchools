4d4a9f51afbb4dc7beed6e8ba70b060198cf1cc4
madhubani15 <119072379+madhubani15@users.noreply.github.com>
2022-12-11 21:27:33 +0530

    Create Class 118-Looping and In Kyeword-CipherSchools.py

---
+++ Class 118-Looping and In Kyeword-CipherSchools.py
@@ -0,0 +1,10 @@
+user_info={
+    'name':'harshit',
+    'age': 24,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+
+for i, j in user_info.items():
+    print(f"key is {i} and value is{j}")
