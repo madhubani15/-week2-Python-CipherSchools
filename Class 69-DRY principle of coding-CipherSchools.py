559ed8f3951b6cfc7b8e15fbea227f1f34836637
madhubani15 <119072379+madhubani15@users.noreply.github.com>
2022-12-11 21:34:10 +0530

    Create Class 69-DRY principle of coding-CipherSchools.py

---
+++ Class 69-DRY principle of coding-CipherSchools.py
@@ -0,0 +1,20 @@
+import random
+wining_number = random.randint(1,100)
+guess = 1
+game_over= False
+
+while True:
+    number = int(input("Guess a number between 1 and 100: "))
+
+    if number == wining_number:
+        print(f"You win and you guessed this number in {guess} times ")
+        break
+    else:
+        if number < wining_number:
+            print("Too Low")
+
+        else:
+            print("Too High")
+
+        guess += 1
+        continue
