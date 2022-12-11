d9a938409739ef987030363a034ebca52e4b7444
madhubani15 <119072379+madhubani15@users.noreply.github.com>
2022-12-11 21:50:57 +0530

    Create Class 97-More about lists-CipherSchools.py

---
+++ Class 97-More about lists-CipherSchools.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3,4,5,6,7,8,9,10,1,5,7,8,1,3]
+# print(numbers)
+# popped_item = numbers.pop()
+# print(numbers)
+# print(numbers.index(1,11))
+def negative_list(l):
+    negative = []
+    for i in l:
+        negative.append(-i)
+    return negative
+print(negative_list(numbers))
