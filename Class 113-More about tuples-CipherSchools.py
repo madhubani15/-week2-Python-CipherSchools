92023f1706de310ef50e059071173ea966bc4574
madhubani15 <119072379+madhubani15@users.noreply.github.com>
2022-12-11 21:24:24 +0530

    Create Class 113-More about tuples-CipherSchools.py

---
+++ Class 113-More about tuples-CipherSchools.py
@@ -0,0 +1,14 @@
+mixed = (1,2,3,3.0)
+
+num = (1)
+words = ('word1')
+
+guiters = 'yamaha', 'button rouge','taylor'
+
+guitarists = ('Maneli Jamal','Eddie Van Der Meer','Andrew Foy')
+guitarist1,guitarist2,guitarist3 = (guitarists)
+
+favourites = ('southern mongolia', ['tokyo ghoul theme','landscape'])
+favourites[1].pop()
+favourites[1].append("We made it")
+print(favourites)
