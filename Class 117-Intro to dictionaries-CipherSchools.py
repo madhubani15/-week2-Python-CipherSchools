691ee6a67d89cc0d3c6b0289431d2b921c6d4e31
madhubani15 <119072379+madhubani15@users.noreply.github.com>
2022-12-11 21:26:43 +0530

    Create Class 117-Intro to dictionaries-CipherSchools.py

---
+++ Class 117-Intro to dictionaries-CipherSchools.py
@@ -0,0 +1,18 @@
+user = {'name':'harshit','age': 24}
+
+user1=dict(name='Harshit', age= 24)
+
+
+user_info={
+    'name':'harshit',
+    'age': 24,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale'],
+
+
+
+
+}
+user_info2= {}
+user_info2['name']='mohit'
+print(user_info2)
